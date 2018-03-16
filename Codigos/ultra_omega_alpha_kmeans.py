3c56c436b797bb31d2ba4b63bfb3dd25a960710d
aum <arthur.duran@br.com.br>
2018-03-16 17:21:37 -0300

    esqueleto do kmeans corre cao

--- Codigos/ultra_omega_alpha_kmeans.py
+++ Codigos/ultra_omega_alpha_kmeans.py
@@ -2,13 +2,14 @@ import numpy as np
 
 class ultra_omega_alpha_kmeans():
 
-    def __init__(self,input = None,no_clusters = None,iniciacao = 'c-means++', algoritmo = 'media',no_iteracoes = 500):
-        self.input = input
-        self.no_clusters=no_clusters
-        self.iniciacao=iniciacao
-        self.algoritmo=algoritmo
+    def __init__(self, dados = None,no_clusters = None,iniciacao = 'c-means++', algoritmo = 'media',no_iteracoes = 500):
+        self.dados = dados
+        self.no_clusters = no_clusters
+        self.iniciacao = iniciacao
+        self.algoritmo = algoritmo
         self.no_iteracoes = no_iteracoes
 
+    #Calcula a proxima matriz
     def __prox_U(self):
         pass
     

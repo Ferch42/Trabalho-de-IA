087ab873d31be6a34a42ac94615193769df8c9da
AugustoCalado <augusto.bueno@usp.br>
2018-04-18 16:48:25 -0300

    silhueta

--- Codigos/Codigos Pos Processamento/Silhueta.py
+++ Codigos/Codigos Pos Processamento/Silhueta.py
@@ -2,12 +2,32 @@
 # Silhueta de um ponto para cada cluster                max{b(i),a(i)}
 # Silhueta media das silhuetas para cada cluster total
 import numpy as np
-import ultra_omega_alpha_kmeans
+#import /../ultra_omega_alpha_kmeans
 import math
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pickle
 
-def calcularSilheta(kmeans):
-    S_dos_cluster = []
 
+def calcularSilhueta(kmeans):
+
+    silhueta = []
+
+    my_cluster =  kmeans.clusters #coordenadas para os dados 
+    my_dados = kmeans.dados # matriz de dados - Para acessar um dado devemos consultar a cordenada em my_cluters (lista de listas)
+
+    conj_daora = criarConjunto(my_cluster, my_dados)
+    
+    #Limpando memória
+    my_cluster = None
+    my_dados = None
+
+    for cluster in len(conj_daora):
+       silhueta.append(SilhuetaDado(cluster, conj_daora))
+
+
+    
+    
    #atributos para kmeans    
     #cluster lista de listas
     #dados matriz de dados
@@ -18,42 +38,74 @@ def calcularSilheta(kmeans):
 
 distancia_euclidiana = lambda x,y: np.sqrt(((x-y)**2).sum())
 
+def criarConjunto(clusters, dados):
+    conj_daora = []
+    i = 0
+    for my_cluster in clusters: #my_clus
+        conj_daora.append([])
+
+        for coordenadas in my_cluster:
+
+            mydado = dados[coordenadas]
+            conj_daora[i].append(mydado)
+
+        i = i+1
+    return conj_daora
 
 #Representa a(i)
 def distanciaMediaIntra(conj_dados): #silhueta para dados 
+       
     resultado = [] #guarda distancia médio de cada dado para os outros dados do conjunto de dados
+
     for dado in conj_dados:
         dist_soma = 0
         for dado_2 in conj_dados:
             dist_soma = dist_soma + distancia_euclidiana(dado,dado_2)
         resultado.append(dist_soma/len(conj_dados)-1)
 
-    return np.array(resultado)
+    return resultado
 
-def distanciaMediaExtra(conj_dados,conj_clusters_externo): #sulhueta para dados extracluster
+def distanciaMediaExtra(numero_meu_cluster,conj_daora): #silhueta para dados extracluster
+
+    conj_dados = conj_daora[numero_meu_cluster] #pegando os dados para o cluster numero_meu_cluster
+
     resultado = []
+
     for dado in conj_dados:
+
         dist_soma = 0
         soma_media_min = math.inf
+        
+        i = 0
+        for clusterI in conj_daora:
 
-        for clusterI in conj_clusters_externo:
+            if i == numero_meu_cluster: # é o cluster do dado que estamos calculando a distancia
+                continue
+
             soma_temp = 0
 
             for dado_externo in clusterI: 
                 soma_temp = soma_temp + distancia_euclidiana(dado,dado_externo)
 
-            soma_temp = soma_temp/len(clusterI)
+            soma_temp = soma_temp/len(clusterI) #calculando a distancia média 
 
             if soma_temp < soma_media_min :
                 soma_media_min = soma_temp
+
+            i = i + 1
+
         resultado.append(soma_media_min)
     
-    return np.array(resultado)
+    return resultado
 
 
-def SilhuetaDado(conj_a, conj_b):
+def SilhuetaDado(cluster_avaliado, conj_daora):
+
+    conj_a = distanciaMediaIntra(conj_daora(cluster_avaliado))
+    conj_b = distanciaMediaExtra(cluster_avaliado,conj_daora)
 
     resultado = []
+
     for a,b in conj_a,conj_b:
         maior_value = 0
         if(a>b):
@@ -62,7 +114,7 @@ def SilhuetaDado(conj_a, conj_b):
             maior_value = b
         
         resultado.append((b-a)/maior_value)
-    return np.array(resultado)
+    return np.array(resultado) #valor de silhueta para cada dado do cluster
 
 def SilhuetaGrupo(conj_silhueta_dados):
     return conj_silhueta_dados.sum()/len(conj_silhueta_dados)
---
+++ Codigos/Codigos Pos Processamento/dadosdor.py
@@ -0,0 +1,29 @@
+import os
+import ultra_omega_alpha_kmeans
+import pickle
+	
+def recupera_dados(dicinfo):
+	
+	while('Trabalho-de-IA' not in os.listdir()):
+		os.chdir('..')
+	os.chdir('Trabalho-de-IA')
+	os.chdir('Objetos')
+
+	if(dicinfo['corpus']=='bbc'):
+		os.chdir('ObjetosPreprocessados')
+	else:
+		os.chdir('Objetos Preprocessados Reuters')
+	os.chdir(dicinfo['representacao'])
+	os.chdir(dicinfo['tamanho'])
+	os.chdir(dicinfo['processamento'])
+
+	print("conversando com os amiguinhos ",os.listdir())
+	amigo=None
+	for a in os.listdir():
+		if(dicinfo['LSA'] and 'LSA' in a):
+			print("lsa otario")
+			amigo=pickle.load(open(a,'rb'))
+		if('LSA' not in a and dicinfo['LSA']==False):
+			print("não lsa otario")
+			amigo=pickle.load(open(a,'rb'))
+	return amigo

7967e8ffe901ae663395ef43ccfef1febb2f2595
cupcakemayhem15 <arthur.um@usp.br>
2018-05-20 19:59:39 -0300

    Plotar silhueta add example

--- Codigos/Codigos Processamento/Means/PlotarSilhueta.py
+++ Codigos/Codigos Processamento/Means/PlotarSilhueta.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import silhuetaDInamico as sd
 import numpy as np
+import ultra_omega_alpha_kmeans as uoak
+import pickle
+import os
+import pathlib
 
 def plotar_silhueta(silhueta_dados):
     fig = plt.figure()
@@ -10,8 +14,18 @@ def plotar_silhueta(silhueta_dados):
     plt.plot()
 
 if __name__ == "__main__":
+    filepath = os.path.realpath(
+                            "../../../Objetos/ObjetosProcessados/Word2Vec/Word2VecLSA.aug")
+    abspath = pathlib.Path(filepath).absolute()
+    with open(abspath, 'rb') as matter:
+        dados = pickle.load(matter)
+    kmeans = uoak.ultra_omega_alpha_kmeans(no_clusters=5, inicializacao='++')
+    kmeans.incluir(dados)
+    kmeans.inicializar()
+    kmeans.executar()
+    silhueta_dados = sd.calcularSilhuetaDados(kmeans)
+    plotar_silhueta(silhueta_dados)    
     
-    pass
 
 
 

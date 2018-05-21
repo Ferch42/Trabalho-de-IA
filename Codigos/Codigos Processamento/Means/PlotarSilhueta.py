b5766bb58faa5ed1ba68f9f3f60c321d19f7466b
AugustoCalado <augusto.bueno@usp.br>
2018-05-20 21:28:20 -0300

    Plotar silhueta primeiro working edition

--- Codigos/Codigos Processamento/Means/PlotarSilhueta.py
+++ Codigos/Codigos Processamento/Means/PlotarSilhueta.py
@@ -9,16 +9,23 @@ import pathlib
 def plotar_silhueta(silhueta_dados):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    Y = np.arange(0, len(silhueta_dados))
-    ax.fill_betweenx(Y, 0, silhueta_dados)
-    plt.plot()
+    flat_silhueta_dados = []
+    for cluster in silhueta_dados:
+        cluster = list(cluster)
+        cluster.sort(key=lambda x: x) #sort ordem decrescente
+        for dado in cluster:
+            flat_silhueta_dados.append(dado)
+    Y = np.arange(0, len(flat_silhueta_dados))
+    ax.fill_betweenx(Y, flat_silhueta_dados)
+    plt.show()
 
 if __name__ == "__main__":
     filepath = os.path.realpath(
-                            "../../../Objetos/ObjetosProcessados/Word2Vec/Word2VecLSA.aug")
+                            "../../../Objetos/ObjetosProcessados/Word2Vec/Word2Vec.aug")
     abspath = pathlib.Path(filepath).absolute()
     with open(abspath, 'rb') as matter:
         dados = pickle.load(matter)
+    dados = np.array(dados)
     kmeans = uoak.ultra_omega_alpha_kmeans(no_clusters=5, inicializacao='++')
     kmeans.incluir(dados)
     kmeans.inicializar()

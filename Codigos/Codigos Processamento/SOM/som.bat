543ed5e72582d278459a2c711668fe67c98e028f
Ferch42 <fernando.hsieh@usp.br>
2018-05-29 11:03:56 -0300

    som.batman

--- Codigos/Codigos Processamento/SOM/som.bat
+++ Codigos/Codigos Processamento/SOM/som.bat
@@ -1,5 +1,3 @@
-python "solta+o+ssssomn.py" TF 0.1
-python "solta+o+ssssomn.py" TF 0.4
 python "solta+o+ssssomn.py" TF 0.7
 python "solta+o+ssssomn.py" TFIDF 0.1
 python "solta+o+ssssomn.py" TFIDF 0.4

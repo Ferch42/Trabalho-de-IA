de32ffde149d06f8143508e8ea27c775e943d4bf
AugustoCalado <augusto.bueno@usp.br>
2018-05-29 21:16:12 -0300

    subindo w2vc bbc

---
+++ Codigos/Codigos Processamento/Means/AugustoApagar.bat
@@ -0,0 +1,9 @@
+@echo off
+cd "path"
+python sevira_no_word2vec_reuters.py Word2Vec 2
+python sevira_no_word2vec_reuters.py Word2Vec 3
+python sevira_no_word2vec_reuters.py Word2Vec 4
+python sevira_no_word2vec_reuters.py Word2Vec 5
+python sevira_no_word2vec_reuters.py Word2Vec 6
+python sevira_no_word2vec_reuters.py Word2Vec 7
+pause 

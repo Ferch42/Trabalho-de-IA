98dc27f51d4965752680a5675f8728977a041655
Ferch42 <fernando.hsieh@usp.br>
2018-05-30 19:48:09 -0300

    word2vec

---
+++ Codigos/Codigos Processamento/SOM/solta_word2vec.bat
@@ -0,0 +1,7 @@
+python solta_o_word2vec.py Word2Vec 0.1
+python solta_o_word2vec.py Word2Vec 0.4
+python solta_o_word2vec.py Word2Vec 0.7
+python solta_o_som_word2vec_reuters.py Word2Vec 0.1
+python solta_o_som_word2vec_reuters.py Word2Vec 0.4
+python solta_o_som_word2vec_reuters.py Word2Vec 0.7
+pause

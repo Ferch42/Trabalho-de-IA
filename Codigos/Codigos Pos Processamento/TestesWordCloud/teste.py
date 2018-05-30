4fc3330a29ed419917ab9cb2af831bd0a34d0e6b
lucaslai10 <lucaslai10>
2018-05-29 21:32:36 -0300

    wordcloud

--- Codigos/Codigos Pos Processamento/TestesWordCloud/teste.py
+++ Codigos/Codigos Pos Processamento/TestesWordCloud/teste.py
@@ -14,7 +14,7 @@ import random
 import Wordcloud
 
 path_arquivos = "../../../Objetos/ObjetosPreProcessados/"
-come_xuchu = pickle.load(open(path_arquivos + 'TFIDF' + "/" + '3k' + "/" + 'Tokenizer' + "/" + 'tfidfVector3kTokenizerLSA.aug' ,"rb"))
+come_xuchu = pickle.load(open(path_arquivos + 'Binario' + "/" + '3k' + "/" + 'Tokenizer' + "/" + 'binaryVector3kTokenizerLSA.aug' ,"rb"))
 if(not isinstance(come_xuchu,np.ndarray)):
 	come_xuchu=np.array(come_xuchu.todense(), dtype = np.float64)
 kmeans = ultra_omega_alpha_kmeans.ultra_omega_alpha_kmeans(no_clusters=5, algoritmo = "media", distancia = "euclidiana")
@@ -22,8 +22,8 @@ kmeans.incluir(come_xuchu)
 kmeans.inicializar()
 kmeans.executar()
 
-n=30
-path='../../../bbc/'
-lista_mais_frequentes = Wordcloud.PalavrasMaisFrequentesCluster.gerar_n_palavras_mais_frequentes_por_cluster(n, path, kmeans)
+n=20
+corpus = 'bbc'
+lista_mais_frequentes = Wordcloud.PalavrasMaisFrequentesCluster.gerar_n_palavras_mais_frequentes_por_cluster(n, corpus, kmeans)
 Wordcloud.plotar_word_cloud(lista_mais_frequentes)
 
